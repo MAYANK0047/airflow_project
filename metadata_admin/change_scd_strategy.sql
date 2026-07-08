32f7d38e645fc90e866fc7290421e3b4a8a0b362
MAYANK0047 <mayankgodara87@gmail.com>
2026-07-08 10:53:26 +0530

    "change SCD type or metadata"

--- metadata_admin/change_scd_strategy.sql
+++ metadata_admin/change_scd_strategy.sql
@@ -1,2 +1,3 @@
--- Validating CI/CD Pipeline connectivity
-SELECT 'Pipeline is operational' AS status;
+UPDATE workspace.default.pipeline_configuration_v2
+SET scd_strategy = 'SCD2' -- (Change this to 'SCD1' if that is your naming convention)
+WHERE target_table = 'gold_employee_dim';
