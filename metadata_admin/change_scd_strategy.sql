9767ce7a9244b5fa95cb167ab13a67b62d03cf5a
MAYANK0047 <mayankgodara87@gmail.com>
2026-07-09 11:27:57 +0530

    ""

--- metadata_admin/change_scd_strategy.sql
+++ metadata_admin/change_scd_strategy.sql
@@ -1,3 +1,3 @@
 UPDATE workspace.default.pipeline_configuration_v2
-SET scd_strategy = 'SCD2' -- (Change this to 'SCD1' if that is your naming convention)
+SET scd_strategy = 'SCD2' 
 WHERE target_table = 'gold_employee_dim';
